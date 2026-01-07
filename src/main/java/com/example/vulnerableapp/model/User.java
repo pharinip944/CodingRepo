9e5545834d9f5c2cc62220d72362e67996a2d5c5
pharinip944 <pharinip944@gmail.com>
2026-01-07 08:50:10 +0530

    Fix: Mark password as transient in User model to avoid accidental serialization/logging.

--- src/main/java/com/example/vulnerableapp/model/User.java
+++ src/main/java/com/example/vulnerableapp/model/User.java
@@ -1,18 +1,10 @@
 package com.example.vulnerableapp.model;
 
-import javax.persistence.*;
-// FIX: Prevent password exposure in API responses
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-@Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
-    // FIX: Prevent password exposure in API responses
-    @JsonIgnore
-    private String password;
+    // FIX: Mark password as transient to avoid accidental serialization/logging
+    private transient String password;
 
     public Long getId() {
         return id;
